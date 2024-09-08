c9378a4ba6676cb742afd049ad84e90e4944d91f
silenc3502 <gcccompil3r@gmail.com>
2024-09-08 22:12:25 +0900

    feat: Navigation Bar의 로그인 버튼 눌러 Github Login 페이지로 이동 [EHFR-5]

--- src/ui/bar/NavigationBar.tsx
+++ src/ui/bar/NavigationBar.tsx
@@ -4,9 +4,15 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavigationBar: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/github-login');
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static"
@@ -32,7 +38,9 @@ const NavigationBar: React.FC = () => {
                             </Typography>
                         </Link>
                     </Box>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit" onClick={handleLogin}>
+                        Login
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
