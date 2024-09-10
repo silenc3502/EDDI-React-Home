b054f546337d3fe6c40901bf9412b4d82992615d
Sanghoon Lee <gcccompil3r@gmail.com>
2024-09-10 17:26:59 +0900

    fix: Github Authorize Code로 Access Token 요청 [EHFR-9]

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ interface Env {
     mode: string | undefined;
     api: {
         MAIN_API_URL: string | undefined;
-        AI_BASE_URL: string | undefined;
+        // AI_BASE_URL: string | undefined;
     };
 }
 
@@ -10,7 +10,7 @@ const env: Env = {
     mode: process.env.NODE_ENV,
     api: {
         MAIN_API_URL: process.env.REACT_APP_MAIN_API_URL,
-        AI_BASE_URL: process.env.REACT_APP_AI_BASE_URL,
+        // AI_BASE_URL: process.env.REACT_APP_AI_BASE_URL,
     },
 };
 
--- src/github_authentication/redirection/GithubRedirectionAuthorizeCode.tsx
+++ src/github_authentication/redirection/GithubRedirectionAuthorizeCode.tsx
@@ -1,6 +1,4 @@
-// src/pages/GithubAuthorizeCode.tsx
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {useRecoilState} from "recoil";
 import {githubAuthenticationState} from "../atom_state/GithubAuthenticationState.";
